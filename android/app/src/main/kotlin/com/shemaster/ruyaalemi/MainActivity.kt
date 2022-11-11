a70aa248ec99709b5612e543174eaee21fa53c3a
Yunus SEVĞANE <yunus@reklamatolyesi.org>
2022-11-12 00:49:52 +0300

    12.11.2022

---
+++ android/app/src/main/kotlin/com/shemaster/ruyaalemi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shemaster.ruyaalemi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/ruyaalemi/shemaster/com/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ruyaalemi.shemaster.com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
